98d87caf438a25b1fa84a813f81cb19b907ed930
Maxim Tsoy <mtsoy1990@gmail.com>
2016-02-21 03:38:45 +0300

    fix tests create startup script

---
+++ startup/start.bat
@@ -0,0 +1,11 @@
+cd ../
+mvn clean package
+start java -Xmx256m -jar eureka-server\target\eurekaserver-0.0.1-SNAPSHOT.jar
+TIMEOUT 20
+start java -Xmx256m -jar config-server\target\configserver-0.0.1-SNAPSHOT.jar
+TIMEOUT 20
+start java -Xmx256m -jar router\target\router-0.0.1-SNAPSHOT.jar
+TIMEOUT 10
+start java -Xmx256m -jar websocket-server\target\websocketserver-0.0.1-SNAPSHOT.jar
+TIMEOUT 10
+start java -Xmx256m -jar websocket-server-without-config-server\target\websocketserverwithoutconfigserver-0.0.1-SNAPSHOT.jar
