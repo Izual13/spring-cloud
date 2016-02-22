e0539492e94f4e510f472569a4d45c75f69a95f0
Maxim Tsoy <mtsoy1990@gmail.com>
2016-02-23 00:23:29 +0300

    fix scripts

--- startup/fast-start.bat
+++ startup/fast-start.bat
@@ -4,7 +4,8 @@ start java -Xmx256m -jar eureka-server\target\eurekaserver-0.0.1-SNAPSHOT.jar
 TIMEOUT 20
 start java -Xmx256m -jar config-server\target\configserver-0.0.1-SNAPSHOT.jar
 TIMEOUT 20
-start java -Xmx256m -jar router\target\router-0.0.1-SNAPSHOT.jar
-start java -Xmx256m -jar message-broker\target\messagebroker-0.0.1-SNAPSHOT.jar
 start java -Xmx256m -jar websocket-server\target\websocketserver-0.0.1-SNAPSHOT.jar
 start java -Xmx256m -jar websocket-server-without-config-server\target\websocketserverwithoutconfigserver-0.0.1-SNAPSHOT.jar
+start java -Xmx256m -jar router\target\router-0.0.1-SNAPSHOT.jar
+TIMEOUT 20
+start java -Xmx256m -jar message-broker\target\messagebroker-0.0.1-SNAPSHOT.jar
