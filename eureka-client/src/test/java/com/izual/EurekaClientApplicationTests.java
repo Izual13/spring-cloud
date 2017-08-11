9639295348e356912f0aa570a07309baec8beb42
Izual <mtsoy1990@gmail.com>
2017-08-11 21:10:28 +0300

    update version spring boot

--- eureka-client/src/test/java/com/izual/EurekaClientApplicationTests.java
+++ eureka-client/src/test/java/com/izual/EurekaClientApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = EurekaClientApplication.class, properties = "server.port=0")
+@SpringBootTest(classes = EurekaClientApplication.class)
 @WebAppConfiguration
 public class EurekaClientApplicationTests {
 
